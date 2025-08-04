00794fbc8013466ce6a4afe6cebb81e8b4a1fb5e
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2025-08-04 22:42:48 +0530

    https://leetcode.com/problems/greatest-common-divisor-of-strings/

---
+++ 1071/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        int length = gcd(str1.length(), str2.length());
+        return str2.substring(0, length);
+    }
+}
